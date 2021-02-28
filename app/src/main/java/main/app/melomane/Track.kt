9b62aa28641f38c524cd49e4d2eb61e30891b217
ericclifford <eric92clifford@gmail.com>
2021-02-27 18:43:36 -0600

    Export Playlist

--- app/src/main/java/main/app/melomane/Track.kt
+++ app/src/main/java/main/app/melomane/Track.kt
@@ -7,7 +7,8 @@ data class Track(
         val id: String,
         val name: String,
         val artist: String,
-        val image: String
+        val image: String,
+        val uri: String
 ) {
     public override fun toString(): String {
         val string = name + " - " + artist
